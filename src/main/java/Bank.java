f172fdd32eff3f997ac49fd09dd9bcdb7a47a947
Joeri Smissaert <joeri@joerismissaert.dev>
2024-04-16 13:00:04 +0300

    Implemented the Bank class

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,4 +1,34 @@
-package main.java;
+import java.util.ArrayList;
 
 public class Bank {
+    private final String bankName;
+    private ArrayList<BankAccount> accounts;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+        this.accounts = new ArrayList<>();
+    }
+
+    public String getBankName() {
+        return this.bankName;
+    }
+
+    public ArrayList<BankAccount> getAccountList() {
+        return this.accounts;
+    }
+
+    public void addAccount(BankAccount account) {
+        this.accounts.add(account);
+    }
+
+    public BankAccount getAccountByNumber(int account) {
+        return this.accounts.getFirst();
+    }
+
+    public void getReport(BankAccount account) {
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Account Holder Name: " + account.getAccountHolderName());
+        System.out.println("Balance: " + account.getBalance());
+        System.out.println("Bank Name: " + this.bankName);
+    }
 }
