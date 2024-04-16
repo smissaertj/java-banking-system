3a55166abf63c39802ca4666a9784f03ec658afa
Joeri Smissaert <joeri@joerismissaert.dev>
2024-04-16 13:38:03 +0300

    Testing basic functionality and InsufficientFundsException

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,29 @@
-package main.java;
+import exceptions.InsufficientFundsExecption;
 
 public class Main {
+
+    public static void main(String[] args) throws InsufficientFundsExecption {
+
+        Bank bank = new Bank("Bank One");
+
+        BankAccount account1 = new BankAccount(1234, "John Doe");
+        bank.addAccount(account1);
+
+        BankAccount account2 = new BankAccount(5678, "Jane Doe");
+        bank.addAccount(account2);
+
+        account1.deposit(500);
+        bank.transferMoney(account1, account2, 500);
+        bank.getReport(account1);
+        bank.getReport(account2);
+
+        int withdrawAmount = 600;
+        if (withdrawAmount > account1.getBalance()) {
+            throw new InsufficientFundsExecption("Insufficient funds");
+        } else {
+            account1.withdraw(withdrawAmount);
+        }
+
+    }
+
 }
--- src/main/java/exceptions/InsufficientFundsExecption.java
+++ src/main/java/exceptions/InsufficientFundsExecption.java
@@ -1,4 +1,4 @@
-package main.java;
+package exceptions;
 
 public class InsufficientFundsExecption {
 }
